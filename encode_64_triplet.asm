e77cf05373c8adca00225aff3debabcbc8528e3b
thiago <thiago.tokarski2001@gmail.com>
2023-02-14 22:31:25 -0300

    base64 table

--- encode_64_triplet.asm
+++ encode_64_triplet.asm
@@ -1,6 +1,11 @@
-        global  encode_64_triplet
-        section .text
-encode_64_triplet:
+section .data
+		symboltable db "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/", 0
+		tablelen db equ $ - symboltable
+
+section .text
+        global _encode_64_triplet
+
+_encode_64_triplet:
         mov     rax, rdi                ; result (rax) initially holds x
         cmp     rax, rsi                ; is x less than y?
         cmovl   rax, rsi                ; if so, set result to y
